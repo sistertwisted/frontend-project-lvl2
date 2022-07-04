c9ea02e43d00316e6b99acb8eb2595208b201a27
Alisa Lobanova <17april94@gmail.com>
2022-07-04 14:14:38 +0300

    remove flat tests and fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,23 +11,10 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
-const test1 = readFile('result_plain');
-
-test('testing getdiffs JSON', () => {
-  const file1 = getFixturePath('before_flat.json');
-  const file2 = getFixturePath('after_flat.json');
-  expect(getDiffs(file1, file2)).toEqual(test1);
-});
-
-test('testing getdiffs YAML', () => {
-  const file1 = getFixturePath('before_flat.yaml');
-  const file2 = getFixturePath('after_flat.yml');
-  expect(getDiffs(file1, file2)).toEqual(test1);
-});
-const test2 = readFile('result');
+const test1 = readFile('result');
 
 test('testing getdiffs stylish', () => {
   const file1 = getFixturePath('before.json');
   const file2 = getFixturePath('after.json');
-  expect(getDiffs(file1, file2)).toEqual(test2);
+  expect(getDiffs(file1, file2)).toEqual(test1);
 });
